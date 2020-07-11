96aa5b8bb736d907f102839a1b59feeca3abc86c
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2020-07-11 20:00:48 +0600

    Working Version With Progress on Top

--- src/github/mjksabit/sabit/gui/controller/Connected.java
+++ src/github/mjksabit/sabit/gui/controller/Connected.java
@@ -1,24 +1,124 @@
 package github.mjksabit.sabit.gui.controller;
 
+import com.jfoenix.controls.JFXAlert;
+import com.jfoenix.controls.JFXProgressBar;
 import github.mjksabit.sabit.core.Connection;
+import github.mjksabit.sabit.core.ftp.IFTP;
+import github.mjksabit.sabit.gui.JFXLoader;
 import github.mjksabit.sabit.gui.model.FileNode;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.Pane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.SocketException;
 import java.util.List;
 
 public class Connected extends Controller {
     Connection connection;
     String fileSaveDirectory = ".";
+
+    private final static int bytePerMB = 1024*1024;
+    private String getInMB(long size) {
+        return String.format("%.3f MB", (double) size / bytePerMB);
+    }
+
+    private class ReceiveProgress implements IFTP.ProgressUpdater {
+        private boolean firstTime = true;
+        private long fileSize;
+
+        @Override
+        public void startProgress(File file) {
+            Platform.runLater(() -> {
+                receiveFileNameText.setText(file.getName());
+                receivePane.setVisible(true);
+            });
+        }
+
+        @Override
+        public void continueProgress(long currentProgress, long totalProgress) {
+            if (firstTime){
+                firstTime = false;
+                fileSize = totalProgress;
+                Platform.runLater(()->receivedTotalInMB.setText(getInMB(totalProgress)));
+            }
+            double percentage = (double) currentProgress / totalProgress;
+            String string = getInMB(currentProgress);
+            Platform.runLater(() -> {
+                receivedInMB.setText(string);
+                receiveProgressBar.setProgress(percentage);
+                receivedPercentage.setText(String.format("%.1f %%",percentage*100));
+            });
+        }
+
+        @Override
+        public void cancelProgress(File file) {
+            Platform.runLater(() -> {
+                receiveProgressBar.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
+                receiveFileNameText.setText("Canceled");
+            });
+        }
+
+        @Override
+        public void endProgress(File file) {
+            Platform.runLater(() -> {
+                transmissionList.add(new FileNode(false, file.getName(), fileSize));
+                receivePane.setVisible(false);
+            });
+        }
+    }
+
+    private class SendProgress implements IFTP.ProgressUpdater {
+        private int index;
+
+        @Override
+        public void startProgress(File file) {
+            Platform.runLater(() -> {
+                sendPane.setVisible(true);
+                sendFileNameText.setText(file.getName());
+                index = transmissionList.size();
+                transmissionList.add(new FileNode(true, file.getName(), file.length()));
+                sendTotalInMB.setText(getInMB(file.length()));
+            });
+        }
+
+        @Override
+        public void continueProgress(long currentProgress, long totalProgress) {
+            double percentage = (double) currentProgress / totalProgress;
+            String string = getInMB(currentProgress);
+            Platform.runLater(() -> {
+                sentInMB.setText(string);
+                sendProgressBar.setProgress(percentage);
+                sentPercentage.setText(String.format("%.1f %%",percentage*100));
+            });
+        }
+
+        @Override
+        public void cancelProgress(File file) {
+            Platform.runLater(() -> {
+                sendProgressBar.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
+                sendFileNameText.setText("Canceled");
+            });
+        }
+
+        @Override
+        public void endProgress(File file) {
+            Platform.runLater(() -> {
+                transmissionList.get(index).markDone();
+                sendPane.setVisible(false);
+            });
+        }
+    }
 
     public void setFileSaveDirectory (String fileSaveDirectory) {
         this.fileSaveDirectory = fileSaveDirectory;
@@ -33,10 +133,10 @@ public class Connected extends Controller {
 
         connection.setFileSaveDirectory(fileSaveDirectory);
 
-        connection.startReceiving(new FileNode(false, transmissionList));
+        connection.startReceiving(new ReceiveProgress());
 
         for (String sendFile : sendFiles)
-            connection.sendFile(new File(sendFile), new FileNode(true, transmissionList));
+            connection.sendFile(new File(sendFile), new SendProgress());
     }
 
     ObservableList<FileNode> transmissionList = FXCollections.observableArrayList();
@@ -50,10 +150,67 @@ public class Connected extends Controller {
     private ListView<FileNode> transferFileList;
 
     @FXML
+    private Label otherUser;
+
+    @FXML
     private Label speedText;
 
     @FXML
-    private Label otherUser;
+    private Pane sendPane;
+
+    @FXML
+    private Label sendFileNameText;
+
+    @FXML
+    private JFXProgressBar sendProgressBar;
+
+    @FXML
+    private Label sentInMB;
+
+    @FXML
+    private Label sendTotalInMB;
+
+    @FXML
+    private Label sentPercentage;
+
+    @FXML
+    private Pane receivePane;
+
+    @FXML
+    private Label receiveFileNameText;
+
+    @FXML
+    private JFXProgressBar receiveProgressBar;
+
+    @FXML
+    private Label receivedInMB;
+
+    @FXML
+    private Label receivedTotalInMB;
+
+    @FXML
+    private Label receivedPercentage;
+
+    @FXML
+    void endShare(ActionEvent event) {
+        if (connection.isActive()) {
+            JFXAlert alert = new JFXAlert(getStage());
+            alert.setContent(new Label("Still Transferring Files, Cancel on both sides!"));
+            alert.setHideOnEscape(true);
+            alert.setHeaderText("Connection Active");
+            alert.show();
+        }
+        else {
+            try {
+                connection.close();
+                Start startPage = JFXLoader.loadFXML("start");
+                startPage.setStage(getStage());
+                startPage.show("Share A BIT");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @FXML
     void addFile(ActionEvent event) {
@@ -63,7 +220,7 @@ public class Connected extends Controller {
 
         if (selectedFiles != null) for (File file : selectedFiles) {
             try {
-                connection.sendFile(file, new FileNode(true, transmissionList));
+                connection.sendFile(file, new SendProgress());
             } catch (FileNotFoundException | SocketException e) {
                 e.printStackTrace();
             }
@@ -77,7 +234,7 @@ public class Connected extends Controller {
         File selectedFolder = directoryChooser.showDialog(getStage());
         if (selectedFolder != null) {
             try {
-                connection.sendFile(selectedFolder, new FileNode(true, transmissionList));
+                connection.sendFile(selectedFolder, new SendProgress());
             } catch (FileNotFoundException | SocketException e) {
                 e.printStackTrace();
             }
--- src/github/mjksabit/sabit/gui/model/FileNode.java
+++ src/github/mjksabit/sabit/gui/model/FileNode.java
@@ -18,21 +18,11 @@ import javafx.scene.layout.Pane;
 import java.io.File;
 import java.io.IOException;
 
-public class FileNode extends AnchorPane implements IFTP.ProgressUpdater{
+public class FileNode extends AnchorPane {
 
     private final static int bytePerMB = 1024*1024;
 
-    private boolean sending = true;
-    private long transferred = 0;
-    private long fileSize = -1;
-
-    private ObservableList<FileNode> list;
-    private int index;
-
-    public FileNode(boolean sending, ObservableList<FileNode> list) {
-        this.sending = sending;
-        this.list = list;
-
+    public FileNode(boolean sending, String filename, long fileSize) {
         try {
             FXMLLoader loader = JFXLoader.getFXMLLoader("file-list-cell");
             loader.setController(this);
@@ -41,16 +31,21 @@ public class FileNode extends AnchorPane implements IFTP.ProgressUpdater{
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        fileNameLabel.setText(filename);
+        totalFileSize.setText(getInMB(fileSize));
+        if (sending)
+            upImage.setVisible(true);
+        else
+            downImage.setVisible(true);
+    }
+
+    public void markDone() {
+        this.setStyle("-fx-background-color: #53ff5c;");
     }
 
     @FXML
     private Label fileNameLabel;
-
-    @FXML
-    private JFXProgressBar progressBar;
-
-    @FXML
-    private Label transferredSize;
 
     @FXML
     private Label totalFileSize;
@@ -65,51 +60,4 @@ public class FileNode extends AnchorPane implements IFTP.ProgressUpdater{
         return String.format("%.3f MB", (double) size / bytePerMB);
     }
 
-    @Override
-    public void startProgress(File file) {
-        Platform.runLater(() -> {
-            if (sending)
-                upImage.setVisible(true);
-            else
-                downImage.setVisible(true);
-            fileNameLabel.setText(file.getName());
-            synchronized (list) {
-                index = list.size();
-                list.add(this);
-            }
-        });
-    }
-
-    @Override
-    public void continueProgress(long currentProgress, long totalProgress) {
-        if (fileSize == -1){
-            fileSize = totalProgress;
-            Platform.runLater( () -> totalFileSize.setText(getInMB(totalProgress)));
-        }
-        Platform.runLater(() -> {
-            transferredSize.setText(getInMB(currentProgress));
-            progressBar.setProgress((double) currentProgress/totalProgress);
-        });
-    }
-
-    @Override
-    public void cancelProgress(File file) {
-        Platform.runLater(() -> {
-            totalFileSize.setText("Canceled");
-            progressBar.setProgress(ProgressIndicator.INDETERMINATE_PROGRESS);
-        });
-    }
-
-    @Override
-    public void endProgress(File file) {
-        Platform.runLater(() -> {
-            transferredSize.setText(getInMB(fileSize));
-            progressBar.setProgress(1);
-
-            synchronized (list) {
-                list.remove(index);
-                list.add(index,this);
-            }
-        });
-    }
 }
